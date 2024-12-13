2375b0bc2f93acf4d95e784f08adc4f6ba9df885
Jeevan Joshi <142773641+JeevanJoshi2061@users.noreply.github.com>
2024-12-13 12:50:04 +0545

    dependicies added

--- CityLink/android/app/src/main/kotlin/com/example/maincitylink/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.maincitylink
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Municipality_panel/android/app/src/main/kotlin/com/example/municipality_panel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.municipality_panel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
